d712a54a0b118c77267e4717543f29389448001d
JohnAsare <johnasare7@gmail.com>
2020-06-25 14:12:33 -0400

    Add the display function

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+# John Asare
+# Jun 25 2020 14:07
+
+
+"""Tic Tac Toe Milestone Project game! """
+
+
+# A function to print out the Tic Tac Toe board
+def display_board(board):
+    pass
+
+
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+display_board(test_board)
