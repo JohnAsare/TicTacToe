4a4890e712628ba8fbb649b81fe4579b11afb33d
JohnAsare <johnasare7@gmail.com>
2020-06-25 18:36:08 -0400

    Ask user what maker do they want to be

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,7 @@
 
 # A function to print out the Tic Tac Toe board
 def display_board(board):
-    print('\n'*100)
+    #print('\n'*100)
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-----')
     print(board[4] + '|' + board[5] + '|' + board[6])
@@ -20,22 +20,23 @@ display_board(test_board)
 
 
 # Ask users for what marker do they want.
-# def player_input():
-#     check = ''
-#
-#     while check != 'X' or check != 'Y':
-#         player1 = input('What maker do you want? (X or Y): ')
-#
-#         if player1 == 'X':
-#             player2 = 'Y'
-#             print(f'Opponent is Y')
-#             break
-#         elif player1 == 'Y':
-#             player2 = 'X'
-#             print(f'Opponent is X')
-#             break
-#     return player1, player2
-# player_input()
+def player_input():
+    check = ''
+
+    while check != 'X' and check != 'O':
+        check = input('What maker do you want to be? (X or O): ')
+
+    player1 = check
+    if player1 == 'X':
+        print('Opponent is O')
+        player2 = 'O'
+    else:
+        print('Opponent is X')
+        player2 = 'X'
+    return player1, player2
+
+
+player_input()
 
 
 # def place_marker(board, marker, position):
