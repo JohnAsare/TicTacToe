2d41f8c3f22757c87a4abe914274d38ea34b1875
JohnAsare <johnasare7@gmail.com>
2020-06-25 17:17:02 -0400

    Ask user what sign do they want to be

--- tictactoe.py
+++ tictactoe.py
@@ -7,10 +7,25 @@
 
 # A function to print out the Tic Tac Toe board
 def display_board(board):
+    print('\n'*100)
     print(board[7] + '|' + board[8] + '|' + board[9])
     print(board[7] + '|' + board[8] + '|' + board[9])
     print(board[7] + '|' + board[8] + '|' + board[9])
 
 
-test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+test_board = [' ']*10
 display_board(test_board)
+
+
+# Ask users for what marker do they want.
+def player_input():
+    marker = ' '
+    while marker != 'X' or marker != 'O':
+        player1 = input('Which maker do you want to be? ("X" or "Y"): ')
+
+        if player1 == 'X':
+            marker = 'X'
+            player2 = 'Y'
+
+
+player_input()
